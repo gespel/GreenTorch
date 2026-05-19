0cf2d2e1f871ef6f6d4d77fa5eed130fdd0847f2
gespel <heimbrodt@uni-potsdam.de>
2026-05-19 10:43:40 +0200

    better

--- green_torch.py
+++ green_torch.py
@@ -4,7 +4,7 @@ from contextlib import ContextDecorator
 class GreenTorch(ContextDecorator):
     def __init__(self, key=0):
         self.logger = logging.getLogger("GreenTorch")
-        logging.basicConfig(level=logging.DEBUG, format="%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+        logging.basicConfig(level=logging.DEBUG, format="%(asctime)s|%(name)s|%(levelname)s: %(message)s")
         self.logger.info("GreenTorch initialized")
         self.key = key
 
