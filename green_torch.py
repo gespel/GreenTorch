7861f6abfc75b2718fa03947ec46c5e83c15a6a8
gespel <heimbrodt@uni-potsdam.de>
2026-05-19 09:38:27 +0200

    working test

--- green_torch.py
+++ green_torch.py
@@ -6,11 +6,14 @@ class GreenTorch(ContextDecorator):
 
     def __enter__(self):
         print("Entering dynamic frequency scaling part!")
+        print(x)
 
     def __exit__(self, exc_type, exc, tb):
         print("Exiting dynamic frequency part!")
 
 
+
 if __name__ == "__main__":
+    x = 1
     with GreenTorch():
         print("This is a test!")
