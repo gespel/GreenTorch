724bb79aa5ba1bab6e232f58e5faa557aabd4919
gespel <heimbrodt@uni-potsdam.de>
2026-05-19 10:42:22 +0200

    adding logging

--- green_torch.py
+++ green_torch.py
@@ -1,19 +1,23 @@
+import logging
 from contextlib import ContextDecorator
 
 class GreenTorch(ContextDecorator):
-    def __init__(self):
-        print("GreenTorch initialized")
+    def __init__(self, key=0):
+        self.logger = logging.getLogger("GreenTorch")
+        logging.basicConfig(level=logging.DEBUG, format="%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+        self.logger.info("GreenTorch initialized")
+        self.key = key
 
     def __enter__(self):
-        print("Entering dynamic frequency scaling part!")
-        print(x)
+        self.logger.info("Entering dynamic frequency scaling part!")
+        self.logger.info(self.key)
 
     def __exit__(self, exc_type, exc, tb):
-        print("Exiting dynamic frequency part!")
+        self.logger.info("Exiting dynamic frequency part!")
 
 
 
 if __name__ == "__main__":
     x = 1
     with GreenTorch():
-        print("This is a test!")
+        print(f"This is a test!")
